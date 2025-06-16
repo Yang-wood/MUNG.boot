526380683c59360bf782b47472f6ab241bb47f64
yangdongeun <xdfs0214@naver.com>
2025-06-16 14:03:37 +0900

    admin과 epub 업로드 연결 완료

---
+++ mung/src/test/java/com/codeit/mini/repository/AdminRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.codeit.mini.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codeit.mini.entity.admin.Admin;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+class AdminRepositoryTest {
+	
+	@Autowired
+	private IAdminRepository adminRepository;
+
+	@Test
+	void testInsertAdmin() {
+		
+		Admin admin = Admin.builder().adminLoginId("admin2")
+									 .adminPw("1234")
+									 .adminName("관리자2")
+									 .adminEmail("admin2@naver.com")
+									 .build();
+		
+		adminRepository.save(admin);
+	}
+
+}
